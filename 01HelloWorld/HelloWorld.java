a6bd9e4da863545bbbef8cb22ed55c5ec42d84a2
JimmyAaron <qfgg700@qq.com>
2023-03-04 21:22:34 +0800

    hello world & code comments

--- 01HelloWorld/HelloWorld.java
+++ 01HelloWorld/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
-    public static void main(String[] args){
+    public static void main(String[] args){//叫做main方法，表示程序主入口
         System.out.println("hello world!");
+        /*叫做输出语句（打印语句）
+        把括号里内容进行输出打印*/
     }
 }
