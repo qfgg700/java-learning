8f2dff4719c74da1beec2a03b6d81b057e54651c
JimmyAaron <qfgg700@qq.com>
2023-03-06 21:46:06 +0800

    Value & Speical Value Demo

---
+++ 02ValueDemo/SpeicalValueDemo.java
@@ -0,0 +1,7 @@
+public class SpeicalValueDemo{
+    public static void main(String[] args){
+        //\t 制表符：在打印的时候把前面字符串的长度补齐到8或8的整数倍
+        System.out.println("name"+"\t"+"age");//4+4+3
+        System.out.println("tom"+"\t"+"23");//3+5+3
+    }
+}
