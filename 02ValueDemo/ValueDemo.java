9e121c7439b8958d05fc228cda973002232252a0
JimmyAaron <qfgg700@qq.com>
2023-03-06 20:59:09 +0800

    ValueDemo

---
+++ 02ValueDemo/ValueDemo.java
@@ -0,0 +1,24 @@
+public class ValueDemo{
+    public static void main(String[] args){
+        //整数
+        System.out.println(666);
+        System.out.println(-123);
+
+        //小数
+        System.out.println(3.14);
+        System.out.println(-9.99);
+
+        //字符串
+        System.out.println("Jimmy Aaron");
+
+        //字符
+        System.out.println('z');
+
+        //布尔类型
+        System.out.println(true);
+        System.out.println(false);
+
+        //空：null不能直接打印
+        System.out.println("null");
+    }
+}
