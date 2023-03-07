f7d51dae2f367556ab4a40936ffa402696488beb
JimmyAaron <qfgg700@qq.com>
2023-03-07 20:49:45 +0800

    Variable

---
+++ 03VariableValueDemo/VariableTest.java
@@ -0,0 +1,13 @@
+public class VariableTest{
+    public static void main(String[] args){
+        int count = 0;
+
+        count = count + 1;
+        count = count + 2 - 1;
+        count = count + 2 - 1;
+        count = count - 1;
+        count = count + 1;
+
+        System.out.println(count);//3
+    }
+}
---
+++ 03VariableValueDemo/VariableValueDemo.java
@@ -0,0 +1,12 @@
+public class VariableValueDemo{
+    public static void main(String[] args){
+        int a = 10;
+        System.out.println(a);
+
+        double b = 3.14;
+        System.out.println(b);
+
+        int x = 100, y = 200, z = 300;
+        System.out.println(x+y+z);
+    }
+}
