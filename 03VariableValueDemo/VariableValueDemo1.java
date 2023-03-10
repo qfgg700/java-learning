5652e49ae4736374566d5a22012b1aa83a1b0bc4
JimmyAaron <qfgg700@qq.com>
2023-03-10 21:55:44 +0800

    基本数据类型

---
+++ 03VariableValueDemo/VariableValueDemo1.java
@@ -0,0 +1,41 @@
+public class VariableValueDemo1{
+    public static void main(String[] args){
+        //byte
+        byte b = 10;
+        System.out.println(b);
+
+        //short
+        short s = 20;
+        System.out.println(s);
+
+        //int
+        int i = 30;
+        System.out.println(i);
+
+        //long
+        /*
+         *如果要定义long类型的变量
+         *在数字后面需要加一个L作为后缀，大小写均可
+         *建议使用大写
+         */
+        long n = 9999999999L;
+        System.out.println(n);
+
+        //float
+        //定义float类型变量的时候需要加F作为后缀
+        float f = 10.1F;
+        System.out.println(f);
+
+        //double
+        double d = 88.8;
+        System.out.println(d);
+
+        //char
+        char c = '好';
+        System.out.println(c);
+
+        //boolean
+        boolean o = true;
+        System.out.println(o);
+    }
+}
---
+++ 03VariableValueDemo/VariableValueDemoTest.java
@@ -0,0 +1,19 @@
+public class VariableValueDemoTest{
+    public static void main(String[] args){
+        String name = "Jimmy Aaron";
+
+        int age = 23;
+
+        char gender = '男';
+
+        double height = 177.5;
+
+        boolean flat = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(height);
+        System.out.println(flat);
+    }
+}
